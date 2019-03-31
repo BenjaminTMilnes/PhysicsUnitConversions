d9d4a470e35e695636b4d48d3b8f287c05fba28c
Benjamin T Milnes <benjamintmilnes@googlemail.com>
2019-04-01 00:04:35 +0100

    Split code into separate files

---
+++ PhysicsUnitConversions.WebApplication/test_framework.js
@@ -0,0 +1,13 @@
+﻿// A microscopic testing framework so that I can run lots of simple tests without faffing around with bigger frameworks
+
+class AssertionError extends Error {
+    constructor(message) {
+        super(message);
+    }
+}
+
+function assertEqual(expectedValue, actualValue) {
+    if (actualValue != expectedValue) {
+        throw new AssertionError("Expected a value of '" + expectedValue + "'; got a value of '" + actualValue + "'.");
+    }
+}
